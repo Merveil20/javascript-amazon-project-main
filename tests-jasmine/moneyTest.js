d4c07cf64e605ae12726dce7f0226d96af9f2c5d
Merveil20 <djudjemerveil20@gmail.com>
2024-12-24 19:45:31 -0400

    Remove Default test and add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,14 @@
+import { FormatCurrency } from '../scripts/utilis/money.js';
+
+describe('test suite: formatCurrency',()=>{
+   it('convert cents into dollars', ()=>{
+    expect(FormatCurrency(2095)).toEqual('20.95');
+   });
+
+   it('works with 0', ()=>{
+    expect(FormatCurrency(0)).toEqual('0.00');
+   });
+   it('rounds up to the nearest cent', ()=>{
+    expect(FormatCurrency(2000.5)).toEqual('20.01');
+   });
+});
